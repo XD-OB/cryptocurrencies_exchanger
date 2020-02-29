2fee4c4eddcc8525f26779fb4d2051a389a24844
XD-OB <ob-96@hotmail.com>
2020-02-29 12:26:24 +0100

    Add API Permission to the realise version

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\1337\Desktop\crypto"
+export "FLUTTER_APPLICATION_PATH=C:\Users\1337\Desktop\flutter_workspace\crypto"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
